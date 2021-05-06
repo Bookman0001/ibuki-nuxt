5c1e1ba8af2715dc47e62358bea786d52fb6e4ca
Bookman0001 <odonel51486666@gmail.com>
2021-05-06 21:13:36 +0900

    chore: add new page

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Now Creating...</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Detail',
+})
+</script>
+
+<style>
+.page-wrapper {
+  background-color: #24140e;
+  padding: 20px;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
